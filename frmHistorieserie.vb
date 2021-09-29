a543a5a32758ed088d44f099cfe532a502dcf4d8
Piet <martijn@kalkhoven.nu>
2021-09-29 14:21:31 +0200

    Loten historie voorbereid in Vissen

--- frmHistorieserie.vb
+++ frmHistorieserie.vb
@@ -20,8 +20,21 @@ Public Class frmHistorieserie
     End Sub
     Private Sub Vulgrid()
 
-        Dim sql = $"SELECT Id, Datum, CAST(IDserieNummer as VARCHAR(2)) + 'e.' as Serie, Plaats FROM DatumWeerEtc WHERE SerieNaamNr = {serie.Id} AND IDseizoen = {seizoen.ID}"
-        Dim dt = Selecteer(sql)
+        Dim sql As String
+
+        Select Case serie.Id
+            Case 1, 2, 3 'Senioren
+                sql = $"SELECT Id, Datum, CAST(IDserieNummer as VARCHAR(2)) + 'e.' as Serie, Plaats FROM DatumWeerEtc WHERE (SerieNaamNr = 1 AND IDseizoen = {seizoen.ID}) OR (SerieNaamNr = 2 AND IDseizoen = {seizoen.ID}) OR (SerieNaamNr = 3 AND IDseizoen = {seizoen.ID})"
+            Case 9, 10, 11 'Winter
+                sql = $"SELECT Id, Datum, CAST(IDserieNummer as VARCHAR(2)) + 'e.' as Serie, Plaats FROM DatumWeerEtc WHERE (SerieNaamNr = 9 AND IDseizoen = {seizoen.ID}) OR (SerieNaamNr = 10 AND IDseizoen = {seizoen.ID}) OR (SerieNaamNr = 11 AND IDseizoen = {seizoen.ID})"
+            Case 12, 13 'Jeugd
+                sql = $"SELECT Id, Datum, CAST(IDserieNummer as VARCHAR(2)) + 'e.' as Serie, Plaats FROM DatumWeerEtc WHERE (SerieNaamNr = 12 AND IDseizoen = {seizoen.ID}) OR (SerieNaamNr = 13 AND IDseizoen = {seizoen.ID})"
+            Case Else 'Alle enkele series
+                'sql = $"SELECT Id, Datum, CAST(IDserieNummer as VARCHAR(2)) + 'e.' as Serie, Plaats FROM Agenda SerieNaamNr = {serie.Id} AND IDseizoen = {seizoen.ID}"
+                sql = $"SELECT Agendaid, Datum, Serie, Locatie FROM Agenda WHERE Serieid = {serie.Id} AND Seizoenid = {seizoen.ID} ORDER BY Datum"
+        End Select
+
+        Dim dt = Selecteeragenda(sql)
 
         dgvloting.DataSource = dt
 
